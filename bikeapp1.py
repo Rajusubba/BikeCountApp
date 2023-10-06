685befa06491055bbc0414968f74226866fce4ee
Rajusubba <subba.raju67@yahoo.com>
2023-10-06 15:25:50 -0400

    Add files via upload

--- bikeapp1.py
+++ bikeapp1.py
@@ -1,10 +1,15 @@
 import streamlit as st
+import sys
+import path
 import pandas as pd
 import numpy as np
 #import sklearn
 #from sklearn.ensemble import GradientBoostingRegressor
 import pickle
-model = pickle.load(open('gb_model.pkl', 'rb'))
+dir = path.Path(_file_).abspath()
+sys.append.path(dir.parent.parent)
+path_to_model = './Users/rajusubba/london-bike-shares-main/gb_model.pkl'
+model = pickle.load(open(path_to_model, 'rb'))
 st.title("Prediction of bike")
 st.markdown("Here we are using temperature as the input to predict the day's revenue")
 st.subheader("Real Temperature in in C")
