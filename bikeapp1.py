adf2396980fad64d635fd7145c09911c7cc8badd
Rajusubba <subba.raju67@yahoo.com>
2023-10-06 13:43:43 -0400

    Add files via upload

--- bikeapp1.py
+++ bikeapp1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import sklearn
+#import sklearn
 from sklearn.ensemble import GradientBoostingRegressor
 import pickle
 model = pickle.load(open('gb_model.pkl', 'rb'))
