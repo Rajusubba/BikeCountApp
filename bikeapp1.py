f9467075be9acc35fb4d5756c0523407a0ec1650
Rajusubba <subba.raju67@yahoo.com>
2023-11-01 20:58:22 -0400

    Add files via upload

--- bikeapp1.py
+++ bikeapp1.py
@@ -1,9 +1,4 @@
 import streamlit as st
-import sys
-import pandas as pd
-import numpy as np
-import sklearn
-from sklearn.ensemble import GradientBoostingRegressor
 import pickle
 #dir = path.Path(_file_).abspath()
 #sys.append.path(dir.parent.parent)
